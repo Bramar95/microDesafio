2c236d1c994341966173c12bef44a6b854063b3f
Bramar95 <bramar2195@gmail.com>
2021-12-14 15:01:11 -0300

    entrega de microdesafio

--- app.js
+++ app.js
@@ -4,12 +4,12 @@ const path= require("path");
 let staticFiles= express.static(path.resolve(__dirname, "public"));
 app.use(staticFiles);
 
-app.listen(3000,()=> console.log("Servidor corriendo en el puerto 3000"));
+app.listen(process.env.PORT || 3000,()=> console.log("Servidor corriendo en el puerto 3000"));
 
 app.get("/", (req, res)=> {
 let htmlPath= path.join(__dirname, "/views/home.html");
 res.sendFile(htmlPath);
-});
+}
 
 app.get("/register", (req, res)=> {
     let registerPath= path.join(__dirname, "/views/register.html");
