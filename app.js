c1c32aa757369065bd2373af50b868624044462a
Bramar95 <bramar2195@gmail.com>
2021-12-15 09:44:15 -0300

    heroku trial

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.listen(process.env.PORT || 3000,()=> console.log("Servidor corriendo en el p
 app.get("/", (req, res)=> {
 let htmlPath= path.join(__dirname, "/views/home.html");
 res.sendFile(htmlPath);
-}
+})
 
 app.get("/register", (req, res)=> {
     let registerPath= path.join(__dirname, "/views/register.html");
